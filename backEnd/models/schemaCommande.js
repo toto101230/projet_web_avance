f96949851ebe4670df602f6ae1939fc711f24b5e
dans300 <danielsierramejia@gmail.com>
2023-10-06 08:13:36 +0200

    commande , reparée

--- backEnd/models/schemaCommande.js
+++ backEnd/models/schemaCommande.js
@@ -3,12 +3,13 @@ const mongoose = require("mongoose");
 const SchemaProd = mongoose.Schema;
 const ObjectId = SchemaProd.ObjectId;
 
+
 const commande = new mongoose.Schema({
     _id: ObjectId,
     utilisateur: { type: String, required: true },
-    listeProduits: { type: [String], required: true },
+    listeProduits:{type: Map, of: String, required: true },
     valide: { type: Boolean, required: true }, // Utilisez 'Number' au lieu de 'Integer'
-});
+}, {strict: false});
 
 
 module.exports = mongoose.model('commande',commande);
